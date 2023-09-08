b81d6b84ba66d458d36bda64c623e481230ca25b
Tyutin_Kirill <kerya.tyutin@yandex.ru>
2023-09-08 17:30:50 +0400

    текста + кнопка заказать

--- components/Header/Header.vue
+++ components/Header/Header.vue
@@ -35,7 +35,10 @@
                         <nuxt-link to="#plintus">Плинтус</nuxt-link>
                     </li>
                     <li>
-                        <nuxt-link to="#dop-raboty">доп. работы</nuxt-link>
+                        <nuxt-link to="#podgotovka-pola">Подготовка пола</nuxt-link>
+                    </li>
+                    <li>
+                        <nuxt-link to="#prodaja-napolnyh-pokrytiy">Продажа покрытий</nuxt-link>
                     </li>
                     <li>
                         <nuxt-link to="#kak_my_rabotaem">как мы работаем</nuxt-link>
@@ -69,190 +72,190 @@
 
 <style lang="scss" scoped>
 .header {
-    position: sticky;
-    top: 0;
-    z-index: 2;
+  position: sticky;
+  top: 0;
+  z-index: 2;
+  background-color: #fff;
+
+  @media screen and (max-width: $MEDIA_TABLET_TO_BIG_MOBILE) {
+    // overflow: hidden;
+  }
+
+  & .container {
+    display: flex;
+    flex-direction: column;
+  }
+
+  &__toggle {
+    position: relative;
+    display: none;
+    border: unset;
+    width: 40px;
+    height: 34px;
     background-color: #fff;
+    border-top: 4px solid $COLOR_ORANGE;
+    border-bottom: 4px solid $COLOR_ORANGE;
+    transition: 0.3s;
 
-        @media screen and (max-width: $MEDIA_TABLET_TO_BIG_MOBILE) {
-            // overflow: hidden;
-        }
+    @media screen and (max-width: $MEDIA_TABLET_TO_BIG_MOBILE) {
+      width: 36px;
+      height: 28px;
+    }
 
-    & .container {
-        display: flex;
+    &::after,
+    &::before {
+      content: "";
+      position: absolute;
+      top: calc(50% - 2px);
+      left: 0;
+      width: 100%;
+      height: 4px;
+      background-color: $COLOR_ORANGE;
+      transition: all 0.3s;
+    }
+
+    &-active {
+      border-color: transparent;
+
+      &::after {
+        transform: rotate(45deg);
+      }
+
+      &::before {
+        transform: rotate(-45deg);
+      }
+    }
+
+    @media screen and (max-width: $MEDIA_TABLET_TO_BIG_MOBILE) {
+      display: block;
+    }
+  }
+
+  &__nav {
+    font-size: 20px;
+    font-family: "Bebas", sans-serif;
+    text-transform: uppercase;
+
+    @media screen and (max-width: $MEDIA_DESKTOP_TO_TABLET) {
+      font-size: 16px;
+    }
+
+    @media screen and (max-width: $MEDIA_TABLET_TO_BIG_MOBILE) {
+      position: absolute;
+      right: 0;
+      top: -300%;
+      width: 100%;
+      padding: 10px;
+      background-color: #fff;
+      font-size: 24px;
+      transition: top 0.3s ease;
+      max-height: calc(100vh - 90px);
+      overflow: scroll;
+
+      &-active {
+        top: 100%;
+      }
+    }
+
+    & ul {
+      display: flex;
+      justify-content: space-between;
+
+      @media screen and (max-width: $MEDIA_TABLET_TO_BIG_MOBILE) {
         flex-direction: column;
-    }
-
-    &__toggle {
-        position: relative;
-        display: none;
-        border: unset;
-        width: 40px;
-        height: 34px;
-        background-color: #fff;
-        border-top: 4px solid $COLOR_ORANGE;
-        border-bottom: 4px solid $COLOR_ORANGE;
-        transition: 0.3s;
-
-        @media screen and (max-width: $MEDIA_TABLET_TO_BIG_MOBILE) {
-            width: 36px;
-            height: 28px;
-        }
-
-        &::after, &::before {
-            content: '';
-            position: absolute;
-            top: calc(50% - 2px);
-            left: 0;
-            width: 100%;
-            height: 4px;
-            background-color: $COLOR_ORANGE;
-            transition: all 0.3s;
-        }
-
-        &-active {
-            border-color: transparent;
-
-            &::after {
-                transform: rotate(45deg);
-            }
-
-            &::before {
-                transform: rotate(-45deg);
-            }
-        }
-
-        @media screen and (max-width: $MEDIA_TABLET_TO_BIG_MOBILE) {
-            display: block;
-        }
-    }
-
-    &__nav {
-        font-size: 20px;
-        font-family: 'Bebas', sans-serif;
-        text-transform: uppercase;
-
-        @media screen and (max-width: $MEDIA_DESKTOP_TO_TABLET) {
-            font-size: 16px;
-        }
-
-        @media screen and (max-width: $MEDIA_TABLET_TO_BIG_MOBILE) {
-            position: absolute;
-            right: 0;
-            top: -300%;
-            width: 100%;
-            padding: 10px;
-            background-color: #fff;
-            font-size: 24px;
-            transition: top 0.3s ease;
-            max-height: calc(100vh - 90px);
-            overflow: scroll;
-
-            &-active {
-                top: 100%;
-            }
-        }
-
-        & ul {
-            display: flex;
-            justify-content: space-between;
-
-            @media screen and (max-width: $MEDIA_TABLET_TO_BIG_MOBILE) {
-                flex-direction: column;
-                align-items: center;
-            }
-        }
-
-        & a {
-            display: block;
-            padding-bottom: 14px;
-            transition: color 0.2s ease;
-
-            &:hover {
-                color: $COLOR_ORANGE;
-
-                @media screen and (min-width: $MEDIA_BIG_MOBILE_TO_TABLET) {
-                    padding-bottom: 12px;
-                    border-bottom: 2px solid $COLOR_ORANGE;
-                }
-            }
-
-        }
-    }
-
-    &__top {
-        display: flex;
-        justify-content: space-between;
-        padding: 20px 0;
-
-        @media screen and (max-width: $MEDIA_TABLET_TO_BIG_MOBILE) {
-            align-items: center;
-            background-color: #fff;
-            z-index: 1;
-        }
-    }
-
-    &__logo {
-        height: 70px;
-
-        & > img {
-            height: 70px;
-        }
-
-        @media screen and (max-width: $MEDIA_TABLET_TO_BIG_MOBILE) {
-            height: 50px;
-
-            & > img {
-                height: 50px;
-            }
-        }
-    }
-
-    &__contacts {
-        display: flex;
         align-items: center;
-        font-family: 'Bebas', sans-serif;
-        font-weight: bold;
+      }
+    }
+
+    & a {
+      display: block;
+      padding-bottom: 14px;
+      transition: color 0.2s ease;
+
+      &:hover {
         color: $COLOR_ORANGE;
 
-        @media screen and (max-width: $MEDIA_TABLET_TO_BIG_MOBILE) {
-            display: none;
+        @media screen and (min-width: $MEDIA_BIG_MOBILE_TO_TABLET) {
+          padding-bottom: 12px;
+          border-bottom: 2px solid $COLOR_ORANGE;
         }
+      }
+    }
+  }
+
+  &__top {
+    display: flex;
+    justify-content: space-between;
+    padding: 20px 0;
+
+    @media screen and (max-width: $MEDIA_TABLET_TO_BIG_MOBILE) {
+      align-items: center;
+      background-color: #fff;
+      z-index: 1;
+    }
+  }
+
+  &__logo {
+    height: 70px;
+
+    & > img {
+      height: 70px;
     }
 
-    &__location {
-        position: relative;
-        display: block;
-        font-size: 22px;
-        margin-right: 60px;
+    @media screen and (max-width: $MEDIA_TABLET_TO_BIG_MOBILE) {
+      height: 50px;
 
-        &::before {
-            position: absolute;
-            content: "";
-            width: 13px;
-            height: 18px;
-            background-image: url('../../static/img/icons/icon_location_orange.svg');
-            background-size: cover;
-            left: -20px;
-            background-repeat: no-repeat;
-        }
+      & > img {
+        height: 50px;
+      }
     }
+  }
 
-    &__phone-list {
-        position: relative;
-        font-size: 30px;
+  &__contacts {
+    display: flex;
+    align-items: center;
+    font-family: "Bebas", sans-serif;
+    font-weight: bold;
+    color: $COLOR_ORANGE;
 
-        &::before {
-            position: absolute;
-            content: "";
-            width: 13px;
-            height: 18px;
-            background-image: url('../../static/img/icons/icon_phone_orange.svg');
-            background-size: contain;
-            top: 4px;
-            left: -20px;
-            background-repeat: no-repeat;
-        }
+    @media screen and (max-width: $MEDIA_TABLET_TO_BIG_MOBILE) {
+      display: none;
     }
+  }
+
+  &__location {
+    position: relative;
+    display: block;
+    font-size: 22px;
+    margin-right: 60px;
+
+    &::before {
+      position: absolute;
+      content: "";
+      width: 13px;
+      height: 18px;
+      background-image: url("../../static/img/icons/icon_location_orange.svg");
+      background-size: cover;
+      left: -20px;
+      background-repeat: no-repeat;
+    }
+  }
+
+  &__phone-list {
+    position: relative;
+    font-size: 30px;
+
+    &::before {
+      position: absolute;
+      content: "";
+      width: 13px;
+      height: 18px;
+      background-image: url("../../static/img/icons/icon_phone_orange.svg");
+      background-size: contain;
+      top: 4px;
+      left: -20px;
+      background-repeat: no-repeat;
+    }
+  }
 }
 </style>
--- components/Service/Service.vue
+++ components/Service/Service.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="service">
-    <div class="anchor" :id="id" />
-    <h2 class="service__title">{{title}}</h2>
+    <div class="anchor" :id="service.id" />
+    <h2 class="service__title">{{service.title}}</h2>
 
-    <div class="service__banner"  :class="{'service__banner-reversed' : reversed}">
-      <img :src="`/img/services/${imagepath}`" :alt="title" class="service__image">
+    <div class="service__banner"  :class="{'service__banner-reversed' : service.reversed}">
+      <img :src="`/img/services/${service.bannerImage}`" :alt="service.title" class="service__image">
       <div class="service__banner-info">
-        <p v-for="text in bannerText" :key="text" class="service__banner-text">{{text}}</p>
+        <p v-for="text in service.bannerText" :key="text" class="service__banner-text">{{text}}</p>
       </div>
     </div>
 
-    <div v-if="additionalText.length" class="container service__additional-text-list">
-      <p v-for="text in additionalText" :key="text" class="service__additional-text">
+    <div v-if="service.additionalText" class="container service__additional-text-list">
+      <p v-for="text in service.additionalText" :key="text" class="service__additional-text">
         {{text}}
       </p>
     </div>
 
-    <div v-if="price.length || cases.length" class="service__collapse-list">
-      <div v-if="price.length" class="service__collapse-item">
+    <div class="service__collapse-list">
+      <div v-if="service.price" class="service__collapse-item">
         <button
           @click="priceIsOpen = !priceIsOpen"
           class="service__collapse-toggle"
@@ -27,10 +27,10 @@
         <Collapse
           :is-open="priceIsOpen"
         >
-          <div v-if="price.length" class="service__price-wrapper">
+          <div v-if="service.price" class="service__price-wrapper">
             <table class="service__price" border="0">
               <tbody>
-                <tr v-for="item in price" :key="item.name">
+                <tr v-for="item in service.price" :key="item.name">
                   <td border="0">{{item.name}}</td>
                   <td border="0">{{item.value}}</td>
                 </tr>
@@ -39,7 +39,7 @@
           </div>
         </Collapse>
       </div>
-      <div v-if="cases.length" class="service__collapse-item">
+      <div v-if="service.cases" class="service__collapse-item">
         <button
           @click="sliderIsOpen = !sliderIsOpen"
           class="service__collapse-toggle"
@@ -50,8 +50,8 @@
           :is-open="sliderIsOpen"
         >
           <div class="service__cases">
-            <VueSlickCarousel v-if="cases.length" v-bind="settings" class="slick-works">
-              <div v-for="(image, imageIndex) in cases" :key="imageIndex">
+            <VueSlickCarousel v-if="service.cases" v-bind="settings" class="slick-works">
+              <div v-for="(image, imageIndex) in service.cases" :key="imageIndex">
                 <div class="slick-works__image-wrap" @click="sliderIndex = imageIndex">
                   <img :src="image.src" :alt="image.alt" class="slick-works__image">
                 </div>
@@ -60,11 +60,15 @@
           </div>
         </Collapse>
       </div>
+      <div class="service__collapse-item">
+        <button class="service__collapse-toggle">Заказать</button>
+      </div>
     </div>
 
 
     <CoolLightBox
-      :items="cases"
+      v-if="service.cases"
+      :items="service.cases"
       :index="sliderIndex"
       @close="sliderIndex = null"
     />
@@ -84,6 +88,12 @@ import 'vue-cool-lightbox/dist/vue-cool-lightbox.min.css'
       CoolLightBox,
     },
     props: {
+      service: {
+        type: Object,
+        default () {
+          return {}
+        }
+      },
       id: {
         type: String,
         default: ''
@@ -175,15 +185,27 @@ import 'vue-cool-lightbox/dist/vue-cool-lightbox.min.css'
     display: flex;
     flex-direction: column;
     margin: 20px 0 0;
-    border: 2px solid $COLOR_GREEN;
     border-radius: 6px;
+    box-shadow: 6px 6px 14px 2px rgba(34, 60, 80, 0.4);
   }
 
   &__collapse-item {
-    border-bottom: 1px solid $COLOR_GREEN;
+    position: relative;
+
+    &::after {
+      position: absolute;
+      bottom: 0;
+      left: 3px;
+      height: 1px;
+      width: calc(100% - 6px);
+      background-color: #05140630;
+      content: "";
+    }
 
     &:last-of-type {
-      border: none;
+      &::after {
+        display: none;
+      }
     }
   }
 
@@ -196,6 +218,11 @@ import 'vue-cool-lightbox/dist/vue-cool-lightbox.min.css'
     text-align-last: left;
     font-size: 22px;
     cursor: pointer;
+    transition: 0.2s ease;
+
+    &:hover {
+      color: $COLOR_ORANGE;
+    }
   }
 
   &__cases {
@@ -251,9 +278,9 @@ import 'vue-cool-lightbox/dist/vue-cool-lightbox.min.css'
   &__image {
     display: block;
     width: 50%;
-    max-height: 500px;
 
     @media (max-width: 1023px) {
+      max-height: 500px;
       width: 100%;
     }
   }
@@ -283,11 +310,11 @@ import 'vue-cool-lightbox/dist/vue-cool-lightbox.min.css'
   &__additional-text-list {
     display: flex;
     flex-direction: column;
-    align-items: center;
     row-gap: 15px;
-  }
-  &__additional-text {
-    text-align: center;
+
+    @media (min-width: 1025px) {
+      padding: 0;
+    }
   }
 }
 </style>
